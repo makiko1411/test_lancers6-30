9a6ae51f90b1ae8ae038d190e57b363530a782f4
makiko <169807134+makiko1411@users.noreply.github.com>
2024-07-06 19:30:11 +0900

    Ectract CSS and HTML: D

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,21 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "./dist"),
+    filename: "main.js",
+    // ファイルネームは、main.jsという名前で出力する
   },
-
   module: {
     rules: [
       {
         test: /\.css/,
         use: [
           {
-            loader: "style-loader",
+            loader: MiniCssExtractPlugin.loader,
           },
           {
             loader: "css-loader",
@@ -21,4 +24,10 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
 };
